c856423eb49200dd6740882c928dc9412af98f5d
imrajeevnayan <immrrobot121@gmail.com>
2025-08-24 18:34:02 +0530

    updated

--- discovery-server/src/main/java/com/ecom/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/ecom/discoveryserver/DiscoveryServerApplication.java
@@ -2,13 +2,15 @@ package com.ecom.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServerApplication.class, args);
-        System.out.println("Chl rha haai Sahi ");
+        System.out.println("Chl rha haai Sahi ");// to check it working or not
 	}
 
 }
