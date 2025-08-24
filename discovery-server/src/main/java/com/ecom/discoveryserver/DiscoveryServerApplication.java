7a33e128528339584ba70731b7d47798b98e38c0
imrajeevnayan <immrrobot121@gmail.com>
2025-08-24 17:33:41 +0530

    update

---
+++ discovery-server/src/main/java/com/ecom/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,14 @@
+package com.ecom.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+        System.out.println("Chl rha haai Sahi ");
+	}
+
+}
